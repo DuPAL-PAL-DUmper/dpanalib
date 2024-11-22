b9eb63c53e55499a1efdc0515f0dbf694409a0ea
Fabio Battaglia <tabaglio@posteo.net>
2024-11-22 09:15:39 +0100

    added some docstrings

--- dpanalib/ic/ic_loader.py
+++ dpanalib/ic/ic_loader.py
@@ -70,21 +70,38 @@ class ICLoader:
                     temp_data = temp_data[elem]
 
         return data
+    
+    @classmethod
+    def _deref_multi(cls, data: dict[str, Any], keys: list[str]) -> TomlData:
+        # Walk the path through data using a list of keys
+        return reduce((lambda d, key: d.get(key, None)), keys, data)
 
     @classmethod
     def rebuild_toml_from_definition(cls, definition: ICDefinition) -> str:
+        """Rebuilds a TOML structure using a loaded ICDefinition
+
+        Args:
+            definition (ICDefinition): IC definition that will be turned into a TOML string
+
+        Returns:
+            str: string representing a TOML structure of the loaded IC definition
+        """        
         return tomli_w.dumps(cls._toml_rebuild(definition))
 
+    
     @classmethod
     def extract_definition_from_buffered_reader(cls, filebuf: BufferedReader) -> ICDefinition:
+        """Reads TOML data linked to the BufferedReader and genrates an ICDefinition from it
+
+        Args:
+            filebuf (BufferedReader): reader pointing to TOML data containing an IC definition
+
+        Returns:
+            ICDefinition: Loaded IC definition
+        """        
         toml_data: dict[str, Any] = tomllib.load(filebuf)
 
         # Build a dictionary containing all the parameters to instantiate an ICDefinition
         ic_def_parameters: dict[str, TomlData] = { k:dv for k, v in cls._TOML_KEY_MAP.items() if (dv:=cls._deref_multi(toml_data, v)) is not None }
 
         return ICDefinition(**ic_def_parameters)
-    
-    @classmethod
-    def _deref_multi(cls, data: dict[str, Any], keys: list[str]) -> TomlData:
-        # Walk the path through data using a list of keys
-        return reduce((lambda d, key: d.get(key, None)), keys, data)
